1487295480f43e1df2890bdd89019fef9a838cdd
Priyanshi <priyanshi.chauhan1234@gmail.com>
2020-06-21 22:50:10 +0530

    overlays added for navbar

--- js/main.js
+++ js/main.js
@@ -33,7 +33,21 @@
 function openNav1() {
 	document.getElementById("myNav1").style.width = "100%";
   }
-  
   function closeNav1() {
 	document.getElementById("myNav1").style.width = "0%";
+  }
+
+  
+function openNav2() {
+	document.getElementById("myNav2").style.left = "0";
+  }
+  function closeNav2() {
+	document.getElementById("myNav2").style.left = "100%";
+  }
+
+  function openNav3() {
+	document.getElementById("myNav3").style.height = "100%";
+  }
+  function closeNav3() {
+	document.getElementById("myNav3").style.height = "0%";
   }
